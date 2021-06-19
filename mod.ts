a2261386477d827c564166cb7e0d65ce90706cb9
Voltrex <mohammadkeyvanzade94@gmail.com>
2021-06-19 23:50:24 +0430

    main: refactor
    Refactored `mod.ts`.

--- mod.ts
+++ mod.ts
@@ -2,51 +2,57 @@
 import { Bot } from './interfaces/Bot.ts';
 
 export class Client {
-	private token: string;
-	private base: string;
-	private ratelimited: boolean;
+  private token: string;
+  private base: string;
+  private ratelimited: boolean;
 
-	constructor(token: string) {
-		this.token = token;
-		this.base = 'https://top.gg/api/';
-		this.ratelimited = false;
-	}
+  constructor(token: string) {
+    Object.defineProperty(this, 'token', {
+      value: token,
+      writable: true,
+      configurable: true,
+      enumerable: false
+    });
+    this.base = 'https://top.gg/api/';
+    this.ratelimited = false;
+  }
 
-	private async handleRequest(method: string, path: string, config?: any) {
-		if (this.ratelimited) throw new Error('You are being ratelimited by the top.gg API.');
+  private async handleRequest(method: string, path: string, config?: any): Promise<any> {
+    if (this.ratelimited) throw new Error('You are being ratelimited by the top.gg API.');
 
-		const response = await fetch(`${this.base}${path}`, {
-			method: method,
-			headers: {
-				Authorization: this.token,
-			},
-			body: config ? JSON.stringify(config) : null,
-		});
+    const response = await fetch(`${this.base}${path}`, {
+      method,
+      headers: {
+        Authorization: this.token,
+      },
+      body: config ? JSON.stringify(config) : undefined,
+    });
 
-		if (response.status === 429) {
-			const timeout = Number(response.headers.get('retry-after')) * 1000;
+    if (response.status === 429) {
+      const timeout = Number(response.headers.get('retry-after')) * 1000;
 
-			this.ratelimited = true;
+      this.ratelimited = true;
 
-			setTimeout(() => {
-				this.ratelimited = false;
-			}, timeout);
+      setTimeout(() => {
+        this.ratelimited = false;
+      }, timeout);
 
-			throw new Error('You are being ratelimited by the top.gg API.');
-		}
+      throw new Error(`You are being ratelimited by the top.gg API. Please retry after: ${timeout}s`);
+    }
 
-		let data: any = null;
+    let data: any = null;
 
-		if (response.headers.get('Content-Type')?.startsWith('application/json')) data = response.json();
-		else data = response.text();
+    if (response.headers.get('Content-Type')?.startsWith('application/json')) data = await response.json();
+    else data = await response.text();
 
-		return data;
-	}
+    return data;
+  }
 
-	async getBot(id: string) {
-		if (id.length === 0) throw new Error('ID cant be empty.');
-		return (await this.handleRequest('GET', `/bots/${id}`)) as Bot;
-	}
+  async getBot(id: string): Promise<Bot> {
+    if (id.length === 0) throw new Error("The 'id' argument cannot be empty");
+
+    return this.handleRequest('GET', `/bots/${id}`)) as Bot;
+  }
 }
 
 export default Client;
