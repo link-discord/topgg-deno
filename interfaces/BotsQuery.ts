9563a104684ce25f61f7d64136969097e6e27e3d
Link <50463727+LINKdiscordd@users.noreply.github.com>
2021-06-19 20:57:28 +0200

    change stuff

--- interfaces/BotsQuery.ts
+++ interfaces/BotsQuery.ts
@@ -1,9 +1,9 @@
-import { Bot } from './Bot.ts'
+import { Bot } from "./Bot.ts";
 
 export interface BotsQuery {
-	limit?: number
-	offset?: number
-	search: { [key in keyof Bot]: string } | string
-	sort?: string
-	fields?: string[] | string
+	limit?: number;
+	offset?: number;
+	search: { [key in keyof Bot]: string } | string;
+	sort?: string;
+	fields?: string[] | string;
 }
--- mod.ts
+++ mod.ts
@@ -1,55 +1,57 @@
 // deno-lint-ignore-file no-explicit-any
-import { Bot } from './interfaces/Bot.ts'
+import { Bot } from "./interfaces/Bot.ts";
 
 export class Client {
-	private token: string
-	private base: string
+	private token: string;
+	private base: string;
 	private ratelimiting: {
-		ratelimited: boolean
-		retyAfter: number
-	}
+		ratelimited: boolean;
+		retyAfter: number;
+	};
 
 	constructor(token: string) {
-		this.token = token
-		this.base = 'https://top.gg/api/'
-		this.ratelimiting = { ratelimited: false, retyAfter: 0 }
+		this.token = token;
+		this.base = "https://top.gg/api/";
+		this.ratelimiting = { ratelimited: false, retyAfter: 0 };
 	}
 
 	private async handleRequest(method: string, path: string, config?: any) {
-		if (this.ratelimiting.ratelimited) throw new Error('You are being ratelimited by the top.gg API.')
+		if (this.ratelimiting.ratelimited)
+			throw new Error("You are being ratelimited by the top.gg API.");
 
 		const response = await fetch(`${this.base}${path}`, {
 			method: method,
 			headers: {
-				Authorization: this.token
+				Authorization: this.token,
 			},
-			body: config ? JSON.stringify(config) : null
-		})
+			body: config ? JSON.stringify(config) : null,
+		});
 
 		if (response.status === 429) {
-			const timeout = Number(response.headers.get('retry-after')) * 1000
+			const timeout = Number(response.headers.get("retry-after")) * 1000;
 
 			setTimeout(() => {
-				this.ratelimiting = { ratelimited: false, retyAfter: 0 }
-			}, timeout)
+				this.ratelimiting = { ratelimited: false, retyAfter: 0 };
+			}, timeout);
 
-			this.ratelimiting = { ratelimited: true, retyAfter: timeout }
+			this.ratelimiting = { ratelimited: true, retyAfter: timeout };
 
-			throw new Error('You are being ratelimited by the top.gg API.')
+			throw new Error("You are being ratelimited by the top.gg API.");
 		}
 
-		let data: any = null
+		let data: any = null;
 
-		if (response.headers.get('Content-Type')?.startsWith('application/json')) data = response.json()
-		else data = response.text()
+		if (response.headers.get("Content-Type")?.startsWith("application/json"))
+			data = response.json();
+		else data = response.text();
 
-		return data
+		return data;
 	}
 
 	async getBot(id: string) {
-		if (id.length === 0) throw new Error('ID cant be empty.')
-		return (await this.handleRequest('GET', `/bots/${id}`)) as Bot
-	}
+		if (id.length === 0) throw new Error("ID cant be empty.");
+		return (await this.handleRequest("GET", `/bots/${id}`)) as Bot;
+    }
 }
 
-export default Client
+export default Client;
